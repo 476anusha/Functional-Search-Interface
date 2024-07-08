857b82a9dc63c2e074f8a6ae39a77eebe26a257a
Anusha Singh <112649189+476anusha@users.noreply.github.com>
2024-07-08 13:59:48 +0530

    completed 1st project

---
+++ scrip.js
@@ -0,0 +1,35 @@
+let dropdownBtn = document.getElementById("drop-text");
+ let span = document.getElementById("span");
+let icon = document.getElementById("icon");
+let list = document.getElementById("list");
+ let input = document.getElementById("search-input");
+let listItems = document.querySelectorAll(".dropdown-list-item");
+
+dropdownBtn.onclick = function () {
+list.classList.toggle("show")
+
+ icon.style.rotate = "-180deg";
+};
+
+window.onclick = function (e) {
+  if (
+    e.target.id !== "drop-text" &&
+    e.target.id !== "icon" &&
+    e.target.id !== "span"
+  ) {
+    list.classList.remove("show");
+    icon.style.rotate = "0deg";
+  }
+ 
+ };
+
+ for (item of listItems) {
+   item.onclick = function (e) {
+    span.innerText = e.target.innerText;
+   if (e.target.innerText == "Everything") {
+      input.placeholder = "Search Anything";
+    } else {
+      input.placeholder = "Search in " + e.target.innerText ;
+   }
+   };
+}
